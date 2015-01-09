d27728738cbbfd708735cb02cea18eeb1d327ebe
Shaun ASHBY <sashby@dfi.ch>
2015-01-09 12:33:19 +0100

    Make dependence on host SSL certs explicit for LS listener.

--- manifests/logstash.pp
+++ manifests/logstash.pp
@@ -97,7 +97,7 @@ class elk::logstash(
       logstash::configfile { 'syslog-tcp-ssl-receiver':
         content => template('elk/etc/logstash/conf.d/syslog-tcp-ssl-receiver.conf.erb'),
         order  => 20,
-      }
+      } -> File["${logstash_ssl_listener_host_cert}"]
     }
 
     logstash::configfile { 'syslog-timestamp-filter':
