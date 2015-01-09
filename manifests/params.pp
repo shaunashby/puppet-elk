4040d0e2881ef7ade5b35b77c6b84c712ae8d154
Shaun ASHBY <sashby@dfi.ch>
2015-01-09 09:37:04 +0100

    Add elk::params class.

---
+++ manifests/params.pp
@@ -0,0 +1,25 @@
+#____________________________________________________________________
+# File: params.pp
+#____________________________________________________________________
+#
+# Author:  <sashby@dfi.ch>
+# Created: 2015-01-08 15:00:30+0100
+# Revision: $Id$
+#
+# Copyright (C) 2015
+#
+#--------------------------------------------------------------------
+
+class elk::params {
+  $ensure = 'present'
+
+  case $::osfamily {
+    'Debian': {}
+    'RedHat': {
+      notice("Apparently you are using the module ${module_name} from ${::osfamily}. Bravo.")
+    }
+    default: {
+      fail("The ${module_name} module is not supported on an ${::osfamily} based system. Sorry.")
+    }
+  }
+}
