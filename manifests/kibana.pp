208e9c83804600d220a00ce602281fa73b50215f
Shaun ASHBY <sashby@dfi.ch>
2015-01-14 11:54:08 +0100

    Manage config.js.

--- manifests/kibana.pp
+++ manifests/kibana.pp
@@ -20,4 +20,10 @@ class elk::kibana($src_root='/tmp',$version='3.1.2', $kibana_dashboard_name='def
 
   $kibana_def_route="/dashboard/file/${kibana_dashboard_name}.json"
 
+  # Core Kibana configuration file (for version 3.x.x). This will set the default dashboard
+  # to load from the landing page and the name of the dashboard:
+  file { 'kibana config.js':
+    path => "${src_root}/src/config.js",
+    content => template('elk/usr/share/kibana/src/config.js.erb'),
+  }
 }
