0b59ca6ed13c1d510299e4b6f4865eadaf774099
Shaun ASHBY <sashby@dfi.ch>
2015-01-14 12:08:00 +0100

    Adjust indentation according to style guide.

--- manifests/kibana.pp
+++ manifests/kibana.pp
@@ -26,7 +26,7 @@ class elk::kibana($src_root='/tmp',$version='3.1.2', $kibana_dashboard_name='def
   # Core Kibana configuration file (for version 3.x.x). This will set the default dashboard
   # to load from the landing page and the name of the dashboard:
   file { 'kibana config.js':
-    path => "${src_root}/src/config.js",
+    path    => "${src_root}/src/config.js",
     content => template('elk/usr/share/kibana/src/config.js.erb'),
   }
 }
