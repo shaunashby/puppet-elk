7ceab2753245599d0a2756dea4c2fc1d079c8a19
Shaun Ashby <Shaun.Ashby@gmail.com>
2016-01-01 10:56:11 +0100

    Update version of Elasticsearch package repo to 1.7.x

--- manifests/elasticsearch.pp
+++ manifests/elasticsearch.pp
@@ -11,11 +11,11 @@
 #--------------------------------------------------------------------
 class elk::elasticsearch() inherits elk::params {
 
-  yumrepo { 'elasticsearch-1.2':
+  yumrepo { 'elasticsearch-1.7':
     ensure   => present,
-    baseurl  => 'http://packages.elasticsearch.org/elasticsearch/1.2/centos',
-    descr    => 'Elasticsearch repository for 1.2.x packages',
-    gpgkey   => 'http://packages.elasticsearch.org/GPG-KEY-elasticsearch',
+    baseurl  => 'http://packages.elastic.co/elasticsearch/1.7/centos',
+    descr    => 'Elasticsearch repository for 1.7.x packages',
+    gpgkey   => 'http://packages.elastic.co/GPG-KEY-elasticsearch',
     gpgcheck => 1,
     enabled  => 1,
   }
@@ -26,7 +26,7 @@ class elk::elasticsearch() inherits elk::params {
       'cluster.name'                         => $elk::params::es_cluster_name,
       'discovery.zen.ping.multicast.enabled' => false,
     },
-  } -> Yumrepo['elasticsearch-1.2']
+  } -> Yumrepo['elasticsearch-1.7']
 
   elasticsearch::instance { $elk::params::es_instance_name:
     config  => {
