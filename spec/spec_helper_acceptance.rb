271b6579123e32090f36a60721640556a211469c
Shaun Ashby <Shaun.Ashby@gmail.com>
2016-01-06 17:18:44 +0100

    Add acceptance helper script.

---
+++ spec/spec_helper_acceptance.rb
@@ -0,0 +1,20 @@
+require 'beaker-rspec'
+
+hosts.each do |host|
+  # Install Puppet
+  on host, install_puppet
+end
+
+RSpec.configure do |c|
+  module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+  module_name = module_root.split('-').last
+
+  # Readable test descriptions
+  c.formatter = :documentation
+
+  # Configure all nodes in nodeset
+  c.before :suite do
+    # Install module
+    puppet_module_install(:source => module_root, :module_name => module_name)
+  end
+end
