ab552f6885e97969a517aa80e239223befa78458
Shaun ASHBY <sashby@dfi.ch>
2016-01-15 15:23:30 +0100

    Add compile tests to spec

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
-describe 'elk' do
 
-  context 'with defaults for all parameters' do
-    it { should contain_class('elk') }
-  end
+describe 'elk' do
+  on_supported_os.each do |os,facts|
+    context "on OS #{os}" do
+      let(:facts) do
+        facts
+      end
+
+      it { is_expected.to compile.with_all_deps }
+
+    end # each OS
+  end # supported OS
 end
